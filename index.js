5e8f314963fd24e154bdebfee3d42cb8c4e2849a
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-09-25 07:17:07 +0100

    fixed vid

--- index.js
+++ index.js
@@ -98,9 +98,11 @@ function gen() {
 
       mergedVideo
         // .input('./video.mp4')
-        .mergeAdd(video[randomint(0, video.length - 1)])
-        .mergeAdd(video[randomint(0, video.length - 1)])
-        .mergeAdd(video[randomint(0, video.length - 1)])
+        .mergeAdd(video[1])
+        .mergeAdd(video[1])
+        // .mergeAdd(video[randomint(0, video.length - 1)])
+        // .mergeAdd(video[randomint(0, video.length - 1)])
+        // .mergeAdd(video[randomint(0, video.length - 1)])
         // .mergeAdd('https://player.vimeo.com/external/291648067.sd.mp4?s=7f9ee1f8ec1e5376027e4a6d1d05d5738b2fbb29&profile_id=164&oauth2_token_id=57447761')
         .outputOptions('-c:v libx264')
         .outputOptions('-pix_fmt yuv420p')
