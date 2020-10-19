556fe97b641e05fb758610e9368fcf5346930968
teezzan <teehazzan@gmail.com>
2020-10-19 07:56:44 +0100

    generated spec ayah

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function makesrt(rec_Url, text) {
     var captions = [
       {
         "start": 0, //Time to show caption in milliseconds
-        "end": (metadata.format.duration + 4) * 1000, //Time to hide caption in milliseconds
+        "end": (metadata.format.duration + 1.2) * 1000, //Time to hide caption in milliseconds
         "text": text //Plain text content
       }
     ];
@@ -67,8 +67,8 @@ function makesrt(rec_Url, text) {
 
 
 function gen() {
-  surah_no = randomint(1, 114);
-  no_ayah = randomint(2, surah[surah_no - 1].count);
+  surah_no =2; //randomint(1, 114);
+  no_ayah =29; //randomint(2, surah[surah_no - 1].count);
   no_reciter = 35;//randomint(0, reciter.length - 1)
   rec_Url = `${reciter[no_reciter].audio_url_bit_rate_64}${parseNum(surah_no)}${parseNum(no_ayah)}.mp3`
   console.log(rec_Url);
