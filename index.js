56800b4831d7892dd5d14978923bfd008d5094fe
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-12-10 08:11:54 +0100

    revert

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const options = {
   }
 };
 
-const url = process.env.APP_URL || 'https://quranvidbot.herokuapp.com:443';
+const url = process.env.APP_URL || 'https://quranvideogen.herokuapp.com:443';
 
 const bot = new TelegramBot(TOKEN, options);
 bot.setWebHook(`${url}/bot${TOKEN}`);
@@ -161,21 +161,10 @@ function gen(surah_no = 55, no_ayah = 56, no_reciter = 35, video_url = "", chatI
 }
 
 
-bot.onText(/\/random/, (msg) => {
+bot.onText(/\/gen/, (msg) => {
   bot.sendMessage(msg.chat.id, `Dear ${msg.from.first_name}, Processing Your Request `);
   let surah_no = randomint(1, 114);
   let no_ayah = randomint(2, surah[surah_no - 1].count);
   let no_reciter = 35;//randomint(0, reciter.length - 1)
   gen(surah_no, no_ayah, no_reciter, video = "", msg.chat.id);
 });
-
-bot.onText(/\/greet/, (msg) => {
-  bot.sendMessage(msg.chat.id, `Hello ${msg.from.first_name}. Welcome`);
-
-});
-
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, `Send \random to generate a random Quran Ayah video`);
-
-});
-
